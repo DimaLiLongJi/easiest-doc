a8c5da335200ac11476b171266aa3e53e9aea0a3
lilongji <woshililongji@163.com>
2019-01-03 17:00:10 +0800

    fix lazy load

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = (env) => {
     output: {
       path: env === 'production' ? path.resolve(__dirname) : path.resolve(__dirname, 'dist'),
       filename: '[name].js',
-      publicPath:  env === 'production' ? '/indiv-doc' : '/',
+      publicPath:  env === 'production' ? '/indiv-doc/' : '/', // 上了生成需要制定这个 要不然懒加载的根路径就错了
     },
   
     resolve: {
